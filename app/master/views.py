c9a5fa25cd977a066f7d2df0e961dc6396334041
Anurag <19914954+shenoy-anurag@users.noreply.github.com>
2022-02-17 23:51:03 -0500

    ci: python runtime upgraded to version 3.9.12, api responses improved

--- app/master/views.py
+++ app/master/views.py
@@ -116,31 +116,59 @@ class Protected(Resource):
         return jsonify({"protected": True})
 
 
+class LoadAgent(Resource):
+    @protected()
+    async def post(self, request):
+        global BOT_AGENT
+        bot_agent = agent.Agent()
+        try:
+            if os.path.exists("./models/20220217-182725-warm-quarter.tar.gz"):
+                # model_path="./models/bot-v1.tar.gz"
+                bot_agent = await load_agent_on_start(
+                    model_path="./models/20220217-182725-warm-quarter.tar.gz",
+                    endpoints=_endpoints,
+                    remote_storage=None,
+                    app=bot_app,
+                    loop=asyncio.get_event_loop()
+                )
+            BOT_AGENT = bot_agent
+            return jsonify({'status': 200, 'message': 'success', 'data': "loading"})
+        except Exception as e:
+            return jsonify({'status': 500, 'message': 'error', 'error': str(e)})
+
+
 class TrainBot(Resource):
     @protected()
     async def post(self, request):
         nlu_files = [NLU_MAIN_FILE, NLU_RULES_FILE, NLU_STORIES_FILE]
-        training_result = train(domain=DOMAIN_FILE, config=CONFIG_FILE, training_files=nlu_files)
-        output_path = training_result.model
-        bot_agent = load_agent_on_start(
-            model_path=output_path,
-            endpoints=_endpoints,
-            remote_storage=None,
-            app=bot_app,
-            loop=asyncio.get_event_loop()
-        )
-        return "training"
+        try:
+            training_result = train(domain=DOMAIN_FILE, config=CONFIG_FILE, training_files=nlu_files)
+            output_path = training_result.model
+            bot_agent = load_agent_on_start(
+                model_path=output_path,
+                endpoints=_endpoints,
+                remote_storage=None,
+                app=bot_app,
+                loop=asyncio.get_event_loop()
+            )
+            return jsonify({'status': 200, 'message': 'success', 'data': "training"})
+        except Exception as e:
+            return jsonify({'status': 500, 'message': 'error', 'error': str(e)})
 
 
 class Chat(Resource):
     @protected()
     async def post(self, request):
-        user_text = request.json.get('chat')
-        sender_id = request.json.get('id')
-        return await BOT_AGENT.handle_text(user_text, sender_id=sender_id)
+        try:
+            user_text = request.json.get('chat')
+            sender_id = request.json.get('id')
+            return await BOT_AGENT.handle_text(user_text, sender_id=sender_id)
+        except Exception as e:
+            return jsonify({'status': 500, 'message': 'error', 'error': str(e)})
 
 
 api.add_resource(Health, '/')
 api.add_resource(Protected, '/protected', endpoint='protected')
 api.add_resource(Chat, '/chat', endpoint='chat')
 api.add_resource(TrainBot, '/train', endpoint='train')
+api.add_resource(LoadAgent, '/load-model', endpoint='load-model')
